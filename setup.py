199191ccbe9a015ff1907692f19f0530f757ea50
thomas550i <thomas550i@gmail.com>
2025-03-07 14:27:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cmenu",
+    version="0.0.1",
+    description="Custom Menu App",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
