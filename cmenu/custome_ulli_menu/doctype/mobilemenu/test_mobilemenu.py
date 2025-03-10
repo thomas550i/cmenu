869cd93bc19841109306146ce9583f1e4733a70a
thomas550i <thomas550i@gmail.com>
2025-03-10 14:51:28 +0530

    folder strucher update

---
+++ cmenu/custome_ulli_menu/doctype/mobilemenu/test_mobilemenu.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, info@c9infotech.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMobileMenu(FrappeTestCase):
+	pass
